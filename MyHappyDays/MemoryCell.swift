77f2ffa933555f0c3652e38240d09ab41f55439d
daxapps <daxapps777@gmail.com>
2017-02-27 07:53:26 -0600

    Connect Interface to Code

---
+++ MyHappyDays/MemoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryCell.swift
+//  MyHappyDays
+//
+//  Created by Jason Crawford on 2/27/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
